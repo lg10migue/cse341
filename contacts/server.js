9de76c28e2dabcb1d2e8dbf96c5cd8e4ad494dd2
lg10migue <lop18028@byui.edu>
2023-05-13 17:28:59 -0600

    CORS.
    Updating cors to work for other methods than GET.

--- contacts/server.js
+++ contacts/server.js
@@ -1,14 +1,15 @@
 const express = require( "express" ) ;
 const routes = require( "./routes" ) ;
 const mongodb = require( "./db/connection" ) ;
+const cors = require( "cors" ) ;
 const port = process.env.PORT || 8080 ;
 const app = express() ;
 
 app.use( express.json() ) ;
-app.use( ( request, response, next ) => {
-    response.setHeader( "Access-Control-Allow-Origin", "*" ) ;
-    next() ;
-} ) ;
+app.use( cors( {
+    origin : [ "http://localhost:8080/", "https://cse341-contacts-frontend.netlify.app/" ],
+    methods : [ "GET", "POST", "PUT", "DELETE" ]
+} ) ) ;
 app.use( "/", routes ) ;
 
 mongodb.initDb( ( err ) => {
