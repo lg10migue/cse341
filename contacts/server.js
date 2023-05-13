b4bdca07438a4e5f09be25397635ae0561d5cac3
lg10migue <lop18028@byui.edu>
2023-05-13 17:44:53 -0600

    Update CORS.
    Update the cors configuration.

--- contacts/server.js
+++ contacts/server.js
@@ -6,10 +6,7 @@ const port = process.env.PORT || 8080 ;
 const app = express() ;
 
 app.use( express.json() ) ;
-app.use( cors( {
-    origin : [ "http://localhost:8080/", "https://cse341-contacts-frontend.netlify.app/" ],
-    methods : [ "GET", "POST", "PUT", "DELETE" ]
-} ) ) ;
+app.use( cors() ) ;
 app.use( "/", routes ) ;
 
 mongodb.initDb( ( err ) => {
